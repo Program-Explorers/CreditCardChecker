c2d4fbf8261b905042ae1589fffc126e8209c9fb
FlashCoder101 <anishwestwindsor@gmail.com>
2020-09-03 14:11:45 -0400

    Second commit

---
+++ src/Checker.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Checker {
+    public static void main(String [] args){
+        String[] company = new String[2];
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Hello");
+
+    }
+}
