d1cd50d5bb1da68cfbe5f921fccfa9f24d141990
FlashCoder101 <anishwestwindsor@gmail.com>
2020-09-03 14:27:18 -0400

    Started adding all of the company names and the amount of numbers on their credit cards

--- src/Checker.java
+++ src/Checker.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Checker {
     public static void main(String [] args){
-        String[] company = new String[2];
+        String[] company = {"Bisa", "Fiscover", "Mercian Spress", "Mastercard"};
         Scanner keyboard = new Scanner(System.in);
         
-        System.out.println("Hello");
-
     }
 }
