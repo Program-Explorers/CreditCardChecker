b465f0911f43efcf68878e3c189d0fbeb40397d3
Programlog <66264617+Programlog@users.noreply.github.com>
2020-09-04 14:38:59 -0400

    Update Checker.java

--- src/Checker.java
+++ src/Checker.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Checker {
 
-    public static boolean isValid(String company_name, int num){
+    public static boolean isValid(String company_name, int[] num){
         Scanner keyboard = new Scanner(System.in);
         System.out.println("\nYou chose " + company_name
                 + "\nEnter the credit card number to verify: ");
         long credit_num = keyboard.nextLong();
         int length = String.valueOf(credit_num).length();
-        return  length == num;
+        return  length == num[0] || length == num[1];
 
     }
 
@@ -17,14 +17,14 @@ public class Checker {
         //initializing variables, Scanner, and Hashtable
         String firstName, lastName, company_name;
         Scanner keyboard = new Scanner(System.in);
-        Hashtable<String, Integer> companies = new Hashtable<String, Integer>();
+        Hashtable<String, int[]> companies = new Hashtable<String, int[]>();
 
 
         //Most popular credit card company names and some sample credit number length values
-        companies.put("visa", 11);
-        companies.put("discover", 16);
-        companies.put("american express", 15);
-        companies.put("mastercard", 16);
+        companies.put("visa", new int[] {11});
+        companies.put("discover", new int[] {16, 17, 18, 19});
+        companies.put("american express", new int[] {15});
+        companies.put("mastercard", new int[] {16});
 
         System.out.print("Please enter your first name: ");
         firstName = keyboard.nextLine();
@@ -40,8 +40,8 @@ public class Checker {
             lastName = keyboard.nextLine();
         }
         System.out.println("\nHello " + firstName + " " + lastName);
-        System.out.println("\nCompanies: " + companies
-                + "\nPlease enter the name of the credit card company from the list above: ");
+        System.out.println("\nCompanies: Visa  Discover  American Express  Mastercard" +
+                "\nPlease enter the name of the credit card company from the list above: ");
         company_name = keyboard.nextLine().toLowerCase();
 
         boolean creditCardValid = isValid(company_name, companies.get(company_name));
