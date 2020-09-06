5e4da4fabc843b2b506aad3c60ee0e67b15b658e
FlashCoder101 <anishwestwindsor@gmail.com>
2020-09-06 14:35:33 -0400

    Added a lot more checks into the program to validate the numbers before putting it into Luhn's algorithm

--- src/Checker.java
+++ src/Checker.java
@@ -1,32 +1,56 @@
 import java.util.*;
 
 public class Checker {
-    private static long CREDIT_NUM;
+    private static String CREDIT_NUM;
+    private static int[] num1;
     private static Scanner keyboard = new Scanner(System.in);
     public static boolean isValid(String company_name, int[] num) {
         int length = 0;
         System.out.println("\nYou chose " + company_name
                 + "\nEnter the credit card number to verify: ");
+        CREDIT_NUM = keyboard.nextLine();
+        length = String.valueOf(CREDIT_NUM).length();
         try {
-            CREDIT_NUM = keyboard.nextLong();
-            length = String.valueOf(CREDIT_NUM).length();
-        } catch (InputMismatchException e) {
-            System.out.println("Your number is way too long");
-        }
-
-        try {
-            return length == num[0] || length == num[1];
-
+            if (length == num[0] || length == num[1]) {
+                num1 = new int[CREDIT_NUM.length()];
+                for (int i = 0; i < CREDIT_NUM.length(); i++) {
+                    num1[i] = CREDIT_NUM.charAt(i) - '0';
+                }
+                switch (company_name) {
+                    case "visa":
+                        return num1[0] == 4;
+                    case "discover":
+                        return num1[0] == 6;
+                    case "mastercard":
+                        return num1[0] == 5;
+                    default:
+                        return num1[0] == 3 && num1[1] == 7 || num1[0] == 3 && num1[1] == 4;
+                }
+            }else {
+                return false;
+            }
         } catch (ArrayIndexOutOfBoundsException e) {
-            return length == num[0];
+            if (length == num[0]) {
+                switch (company_name) {
+                    case "visa":
+                        return num1.length == 16 || num1.length == 18 || num1.length == 19;
+                    case "discover":
+                        return num1.length == 16 || num1.length == 19;
+                    case "mastercard":
+                        return num1.length == 16;
+                    default:
+                        return num1.length == 15;
+                }
+            } else {
+                return false;
+            }
 
         }
     }
-
-    public static int evenPlace(long number){
+    public static int evenPlace(String number1){
+        long number = Long.parseLong(number1);
         int totalSum = 0;
         String num = number + "";
-        System.out.println(num);
         for (int i = num.length() - 2; i >= 0; i -= 2)
             totalSum += getDigit(Integer.parseInt(num.charAt(i) + "") * 2);
 
@@ -64,15 +88,16 @@ public class Checker {
             lastName = keyboard.nextLine();
         }
 
+
         System.out.println("\nHello " + firstName + " " + lastName);
         System.out.println("\nCompanies: Visa  Discover  American Express  Mastercard" +
                 "\nPlease enter the name of the credit card company from the list above: ");
         company_name = keyboard.nextLine().toLowerCase();
 
         boolean creditCardValid = isValid(company_name, companies.get(company_name));
-
+         System.out.println(num1[2]);
         if (creditCardValid) {
-            System.out.print(evenPlace(CREDIT_NUM));
+            System.out.println(evenPlace(CREDIT_NUM));
 
         }
         else {
