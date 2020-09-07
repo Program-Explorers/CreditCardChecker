af39a7958f5cb907dcb6e31dc868a1d97e38dc87
FlashCoder <anishwestwindsor@gmail.com>
2020-09-06 22:26:18 -0400

    reformatted file& optimized imports. Made runLuhhAlgo method to run the the algo on the credit card num. And improved user interface with more text, title casing and more

--- src/Checker.java
+++ src/Checker.java
@@ -1,13 +1,17 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 public class Checker {
+    private static final Scanner keyboard = new Scanner(System.in);
     private static String CREDIT_NUM;
     private static int[] num1;
-    private static Scanner keyboard = new Scanner(System.in);
-    public static boolean isValid(String company_name, int[] num) {
-        int length = 0;
-        System.out.println("\nYou chose " + company_name
-                + "\nEnter the credit card number to verify: ");
+
+    public static boolean isValid(String titledName, String company_name, int[] num) {
+        int length;
+        System.out.println("\nYou chose " + titledName
+                + "\nEnter the credit card number to verify (NoSpaces): ");
         CREDIT_NUM = keyboard.nextLine();
         length = String.valueOf(CREDIT_NUM).length();
         try {
@@ -26,7 +30,7 @@ public class Checker {
                     default:
                         return num1[0] == 3 && num1[1] == 7 || num1[0] == 3 && num1[1] == 4;
                 }
-            }else {
+            } else {
                 return false;
             }
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -47,7 +51,8 @@ public class Checker {
 
         }
     }
-    public static int evenPlace(String number1){
+
+    public static int evenPlace(String number1) {
         long number = Long.parseLong(number1);
         int totalSum = 0;
         String num = number + "";
@@ -56,6 +61,7 @@ public class Checker {
 
         return totalSum;
     }
+
     public static int getDigit(int number) {
         if (number < 9) {
             return number;
@@ -63,10 +69,30 @@ public class Checker {
         return number / 10 + number % 10;
     }
 
+    public static boolean runLuhnAlgo(String str) {
+        int[] ints = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            ints[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        for (int i = ints.length - 2; i >= 0; i = i - 2) {
+            int j = ints[i];
+            j = j * 2;
+            if (j > 9) {
+                j = j % 10 + 1;
+            }
+            ints[i] = j;
+        }
+        int sum = 0;
+        for (int i = 0; i < ints.length; i++) {
+            sum += ints[i];
+        }
+        return sum % 10 == 0;
+    }
+
     public static void main(String[] args) {
         //initializing variables, Scanner, and Hashtable
         String firstName, lastName, company_name;
-        HashMap<String, int[]> companies = new HashMap<String, int[]>();
+        HashMap<String, int[]> companies = new HashMap<>();
 
         //Most popular credit card company names and some sample credit number length values
         companies.put("visa", new int[]{16});
@@ -80,6 +106,7 @@ public class Checker {
             System.out.print("\n\nPlease enter your first name: ");
             firstName = keyboard.nextLine();
         }
+        List<String> companyNamesTitled = Arrays.asList("Visa", "Discover", "American Express", "MasterCard");
 
         System.out.print("Please enter your last name: ");
         lastName = keyboard.nextLine();
@@ -93,16 +120,21 @@ public class Checker {
         System.out.println("\nCompanies: Visa  Discover  American Express  Mastercard" +
                 "\nPlease enter the name of the credit card company from the list above: ");
         company_name = keyboard.nextLine().toLowerCase();
+        String titledName = "Mastercard";
+        if (company_name.toUpperCase().equals(companyNamesTitled.get(0).toUpperCase()))
+            titledName = "Visa";
+        else if (company_name.toUpperCase().equals(companyNamesTitled.get(1).toUpperCase()))
+            titledName = "Discover";
+        else if (company_name.toUpperCase().equals(companyNamesTitled.get(2).toUpperCase()))
+            titledName = "American Express";
 
-        boolean creditCardValid = isValid(company_name, companies.get(company_name));
-         System.out.println(num1[2]);
+        boolean creditCardValid = isValid(titledName, company_name, companies.get(company_name));
         if (creditCardValid) {
-            System.out.println(evenPlace(CREDIT_NUM));
-
-        }
-        else {
+            System.out.println("The sum of all the even digits = " + evenPlace(CREDIT_NUM));
+            System.out.println("Your credit card is valid... for now");
+            System.out.println("Your credit card number " + CREDIT_NUM + " is: " + runLuhnAlgo(CREDIT_NUM) + " according to the Luhn algo");
+        } else {
             System.out.println("\nYour credit card is invalid");
         }
-
     }
 }
